3beb4a729629724bf464ca6b75eac286c8e5e462
Junsung Yoon <47721775+junbyeol@users.noreply.github.com>
2026-06-01 20:37:17 +0900

    Time: 1710 ms (56.53%), Space: 19.3 MB (77.92%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = [i*i for i in range(1,101)]
+        dp = [-1] * 10001
+
+        for num in range(1, n+1):
+            if num in squares:
+                dp[num] = 1
+            else:    
+                min_val = dp[num-1] + 1
+                it = 0
+                while squares[it] < num:
+                    min_val = min(dp[num - squares[it]] + 1, min_val)
+                    it = it + 1
+                
+                dp[num] = min_val
+
+            # print(f'num: {num}, dp[num]: {dp[num]}')
+
+        return dp[n]
